89efb76af80080db099bdaee4e8c92d7aaaefdca
Ian Dodds <ianmitchelldodds@gmail.com>
2022-12-16 15:45:51 +0000

    Add methods to change players and handle wrong answers

--- game.rb
+++ game.rb
@@ -7,4 +7,19 @@ class Game
     @players = [player1.name, player2.name]
     @current_player = players[0]
   end
+
+  def change_player
+    if @current_player == @players[0]
+      @current_player = @players[1]
+    else @current_player = @players[0]
+    end
+  end
+      
+  def wrong_answer(player)
+    if player == @player1.name
+      @player1.score = @player1.score - 1
+    elsif player == @player2.name
+      @player2.score = @player2.score - 1
+    end
+  end
 end
